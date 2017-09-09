e22f0b02112d7e7dda4b1050d8ee2f2714f14aab
code <1165444511@qq.com>
2017-09-09 17:58:26 +0800

    if spaceInfo.json does nit exist create and init data int spaceInfo.json

--- start.js
+++ start.js
@@ -1,9 +1,43 @@
 var fs = require('fs');
 
-fs.stat('./initInformation/spaceInfo.json', function(err, stat){
-    if(stat&&stat.isFile()) {
+fs.stat('./initInformation/spaceInfo.json', function (err, stat) {
+    if (stat && stat.isFile()) {
         console.log('文件存在');
     } else {
-        console.log('文件不存在或不是标准文件');
+        var spaceInfoData = `{
+  "spaceInfo": [
+    {
+      "A": {
+        "bookInfo": [],
+        "subtotal": 0
+      }
+    },
+    {
+      "B": {
+        "bookInfo": [],
+        "subtotal": 0
+      }
+    },
+    {
+      "C": {
+        "bookInfo": [],
+        "subtotal": 0
+      }
+    },
+    {
+      "D": {
+        "bookInfo": [],
+        "subtotal": 0
+      }
+    }
+  ],
+  "total": 0
+}`;
+        fs.writeFile('./initInformation/spaceInfo.json', spaceInfoData, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+            return;
+        })
     }
 });
