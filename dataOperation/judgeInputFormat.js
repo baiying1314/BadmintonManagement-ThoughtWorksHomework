5250a6aaacea11b450a1e29799c7a2761aea097a
code <1165444511@qq.com>
2017-09-09 21:42:30 +0800

    get user input in start.js and judge type in judgeInputFormat.js

---
+++ dataOperation/judgeInputFormat.js
@@ -0,0 +1,20 @@
+function judgeInputFormat(inputString) {
+    var regBook = /^\w{3,5}\s[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s+(20|21|22|23|[0-1]\d):00~(20|21|22|23|[0-1]\d):00\s[A|B|C|D]$/;
+    var regBookCancel = /^\w{3,5}\s[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s+(20|21|22|23|[0-1]\d):00~(20|21|22|23|[0-1]\d):00\s[A|B|C|D]\s[C]$/;
+    var regSummary = /^\s$/;
+    if (regBook.test(inputString)) {
+        console.log('book');
+    }
+    else if (regBookCancel.test(inputString)) {
+        console.log('cancel')
+    }
+    else if (regSummary.test(inputString)) {
+        console.log('sumarry')
+    }
+    else {
+        console.log('err');
+    }
+
+}
+
+module.exports = judgeInputFormat;
--- start.js
+++ start.js
@@ -1,29 +1,39 @@
 var fs = require('fs');
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 var statFile = require('./fileOperation/statFile');
 var writeFile = require('./fileOperation/writeFile');
-
 var dataInfo = require('./dataInfo/data');
+var judgeInputFormat = require('./dataOperation/judgeInputFormat');
+
 
 statFile('./initInformation/spaceInfo.json', (statSpaceResult)=> {
     statFile('./initInformation/chargeInfo.json', (statChargeResult) => {
         if (statSpaceResult && statChargeResult) {
-            console.log('success');
+            rl.on('line', (aswer)=> {
+                judgeInputFormat(aswer);
+            })
         }
         else {
             if (!statChargeResult) {
+
                 var chargesInfoData = dataInfo.chargeInfo;
+
                 writeFile('./initInformation/chargeInfo.json', chargesInfoData, (writeChargeResult)=> {
                     if (!writeChargeResult) {
                         console.log('err');
                     }
                     else {
                         console.log('write ChargeInfo successfully');
-
                     }
                 });
             }
             if (!statSpaceResult) {
+
                 var spaceInfoData = dataInfo.spaceInfo;
 
                 writeFile('./initInformation/spaceInfo.json', spaceInfoData, (writeSpaceResult) => {
@@ -32,7 +42,6 @@ statFile('./initInformation/spaceInfo.json', (statSpaceResult)=> {
                     }
                     else {
                         console.log('write spaceInfo successfully');
-
                     }
                 });
             }
