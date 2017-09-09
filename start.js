e333e33bb07d830c49da90fd571adcc839f87196
code <1165444511@qq.com>
2017-09-09 19:33:59 +0800

    optimize code in start.js

--- start.js
+++ start.js
@@ -6,47 +6,36 @@ var writeFile = require('./fileOperation/writeFile');
 var dataInfo = require('./dataInfo/data');
 
 statFile('./initInformation/spaceInfo.json', (statSpaceResult)=> {
-    if (statSpaceResult) {
-        statFile('./initInformation/chargeInfo.json', (statChargeResult) => {
-            if (statChargeResult) {
-                console.log('文件存在');
-            }
-            else {
+    statFile('./initInformation/chargeInfo.json', (statChargeResult) => {
+        if (statSpaceResult && statChargeResult) {
+            console.log('success');
+        }
+        else {
+            if (!statChargeResult) {
                 var chargesInfoData = dataInfo.chargeInfo;
                 writeFile('./initInformation/chargeInfo.json', chargesInfoData, (writeChargeResult)=> {
-                    if (writeChargeResult) {
+                    if (!writeChargeResult) {
+                        console.log('err');
+                    }
+                    else {
                         console.log('write ChargeInfo successfully');
+
+                    }
+                });
+            }
+            if (!statSpaceResult) {
+                var spaceInfoData = dataInfo.spaceInfo;
+
+                writeFile('./initInformation/spaceInfo.json', spaceInfoData, (writeSpaceResult) => {
+                    if (!writeSpaceResult) {
+                        console.log('err');
                     }
                     else {
-                        console.log('write ChargeInfo unsuccessfully');
+                        console.log('write spaceInfo successfully');
+
                     }
-                })
+                });
             }
-        })
-    } else {
-        var spaceInfoData = dataInfo.spaceInfo;
-        writeFile('./initInformation/spaceInfo.json', spaceInfoData, (writeSpaceResult) => {
-            if (writeSpaceResult) {
-                statFile('./initInformation/chargeInfo.json', (statChargeResult) => {
-                    if (statChargeResult) {
-                        console.log('文件存在');
-                    }
-                    else {
-                        var chargesInfoData = `{"weekCharges":{"one":30,"two":50,"three":80,"four":60},"weekendCharges":{"one":40,"two":50,"three":60}}`;
-                        writeFile('./initInformation/chargeInfo.json', chargesInfoData, (writeChargeResult)=> {
-                            if (writeChargeResult) {
-                                console.log('write ChargeInfo successfully');
-                            }
-                            else {
-                                console.log('write ChargeInfo unsuccessfully');
-                            }
-                        })
-                    }
-                })
-            }
-            else {
-                console.log('err');
-            }
-        })
-    }
+        }
+    });
 });
