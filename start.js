09df54559398a9723596a759b286b7faa6e3d31e
code <1165444511@qq.com>
2017-09-09 18:18:55 +0800

    judge if chargeInfo.json exists if not create and init chargeInfo

--- start.js
+++ start.js
@@ -2,7 +2,20 @@ var fs = require('fs');
 
 fs.stat('./initInformation/spaceInfo.json', function (err, stat) {
     if (stat && stat.isFile()) {
-        console.log('文件存在');
+        fs.stat('./initInformation/chargesInfo.json', function (err, stat) {
+            if (stat && stat.isFile()) {
+                console.log('存在')
+            }
+            else {
+                var chargesInfoData = `{"weekCharges":{"one":30,"two":50,"three":80,"four":60},"weekendCharges":{"one":40,"two":50,"three":60}}`;
+                fs.writeFile('./initInformation/chargeInfo.json', chargesInfoData, function (err) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    return;
+                })
+            }
+        })
     } else {
         var spaceInfoData = `{
   "spaceInfo": [
