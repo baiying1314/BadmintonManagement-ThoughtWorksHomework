e19a94983a41c8ad1cc6d411880cd9372db8c5f0
code <1165444511@qq.com>
2017-09-10 13:30:13 +0800

    optimize code extract function in bookBadmiton.js

--- dataOperation/bookBadminton.js
+++ dataOperation/bookBadminton.js
@@ -2,40 +2,71 @@ var fs = require('fs');
 
 var writeFile = require('../fileOperation/writeFile');
 var readFile = require('../fileOperation/readFile');
-function bookBadminton(inputString) {
 
-    var bookInputArr = inputString.split(' ');
-    var inputStartTime = bookInputArr[2].split('~')[0];
-    var inputEndTime = bookInputArr[2].split('~')[1];
-    if (inputEndTime > inputStartTime && (inputStartTime >= '09:00' && inputEndTime <= '22:00')) {
-        readFile('./initInformation/spaceInfo.json', 'utf8', (spaceInfo)=> {
-                var selectSpace = bookInputArr[bookInputArr.length - 1];
-                var correspondSpace = spaceInfo[selectSpace];
-                var selectBookInfo = correspondSpace.bookInfo;
-                for (var i = 0; i < selectBookInfo.length; i++) {
-                    var selectBookArr = selectBookInfo[i].split(' ');
-                    if (bookInputArr[1] === selectBookArr[1]) {
-                        var selectItemStartTime = selectBookArr[2].split('~')[0];
-                        var selectItemEndTime = selectBookArr[2].split('~')[1];
-                        if (!(inputEndTime < selectItemStartTime || inputStartTime > selectItemEndTime)) {
-                            console.log('Error: the booking conflicts with existing bookings!');
-                            break;
-                        }
+function bookBadminton(inputString) {
+    var inputDataObj = splitInputString(inputString);
+    var ifInputRight = judgeInputRight(inputDataObj);
+    if (ifInputRight) {
+        judgeConflict(inputDataObj, (ifConflict)=> {
+            if (!ifConflict) {
+                console.log('Error: the booking conflicts with existing bookings!');
+            }
+            else {
+                writeBookToFile(ifConflict, inputDataObj.space,inputString);
+            }
+        });
+    }
+}
+
+function splitInputString(spaceString) {
+    var bookInputArr = spaceString.split(' ');
+    var userId = bookInputArr[0];
+    var date = bookInputArr[1];
+    var startTime = bookInputArr[2].split('~')[0];
+    var endTime = bookInputArr[2].split('~')[1];
+    var space = bookInputArr[3];
+    return {userId, date, startTime, endTime, space}
+}
+
+function judgeInputRight(inputDataObj) {
+    if (inputDataObj.endTime > inputDataObj.startTime && (inputDataObj.startTime >= '09:00' && inputDataObj.endTime <= '22:00')) {
+        return true;
+    }
+    return false;
+}
+
+function judgeConflict(inputDataObj, callback) {
+    readFile('./initInformation/spaceInfo.json', 'utf8', (spaceInfo)=> {
+            var selectSpace = inputDataObj.space;
+            var correspondSpace = spaceInfo[selectSpace];
+            var selectBookInfo = correspondSpace.bookInfo;
+
+            for (var i = 0; i < selectBookInfo.length; i++) {
+                var selectBook = splitInputString(selectBookInfo[i]);
+                if (inputDataObj.date === selectBook.date) {
+                    var selectItemStartTime = selectBook.startTime;
+                    var selectItemEndTime = selectBook.endTime;
+                    if (!(inputDataObj.endTime < selectItemStartTime || inputDataObj.startTime > selectItemEndTime)) {
+                        return callback(false);
                     }
                 }
-                if (i >= selectBookInfo.length) {
-                    selectBookInfo.push(inputString);
-                    var updateDataJson = JSON.stringify(spaceInfo);
-
-                    writeFile('./initInformation/spaceInfo.json', updateDataJson, (writeResult)=> {
-                        if (writeResult) {
-                            console.log('Success: the booking is accepted!');
-                        }
-                    });
-                }
             }
-        );
-    }
+            if (i >= selectBookInfo.length) {
+                return callback(spaceInfo);
+            }
+        }
+    );
+}
+
+function writeBookToFile(spaceInfo, selectSpace,inputString) {
+    console.log(selectSpace);
+    spaceInfo[selectSpace].bookInfo.push(inputString);
+    var updateDataJson = JSON.stringify(spaceInfo);
+    writeFile('./initInformation/spaceInfo.json', updateDataJson, (writeResult)=> {
+        if (writeResult) {
+            console.log('Success: the booking is accepted!');
+        }
+    });
 }
 
 module.exports = bookBadminton;
